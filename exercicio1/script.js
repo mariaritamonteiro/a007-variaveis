f2a9f1bd94869191bd310f2e6cdf517314b665ee
mariaritamonteiro <mariaritamonteiro14.1@gmail.com>
2022-10-22 13:10:41 -0300

    Entrega de exercícios de variaveis

---
+++ exercicio1/script.js
@@ -0,0 +1,31 @@
+
+
+  //#RESOLUÇÃO PARTE 1
+
+let nome = prompt("Qual o seu nome?")
+let idade =Number(prompt("qual sua idade?"))
+
+let nome1 = "Maria Rita"
+let idade2 = 25
+
+console.log(typeof nome1) 
+console.log(typeof idade2)
+
+// Por que eu não havia declarado um valor para cada variavél.
+
+console.log("olá meu nome é", nome1, "tenho", idade2, "anos")
+
+  //#RESOLUÇAÕ PARTE 2
+
+let perguntaA=prompt("Você tem filhos?")
+let perguntaB=prompt("você tem Pets?")
+let perguntaC=prompt("Você sabe dançar?")
+
+perguntaA = "Você tem filhos?"
+perguntaB = "Você tem Pets?"
+perguntaC = "Você sabe dançar?"
+
+console.log(perguntaA, false)
+console.log(perguntaB, true) 
+console.log(perguntaC, true)
+
